bdf40934fd081a63933d7569eb1efac1f3aed521
Robert Krahn <robert.krahn@gmail.com>
2024-05-01 20:15:09 +0200

    cleanup

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
       let
         pkgs = import nixpkgs { inherit system; };
         cudatoolkit = pkgs.cudaPackages.cudatoolkit;
-        cuda_cudart_static = pkgs.cudaPackages.cuda_cudart.static;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -19,17 +18,6 @@
           ];
 
           LD_LIBRARY_PATH=/run/opengl-driver/lib;
-          # NVCC_APPEND_FLAGS="${cuda_cudart_static}/lib";
-
-          # shellHook = ''
-          #   export CC=gcc-12
-          #   export CXX=g++-12
-          #   export CUDA_PATH=${cudatoolkit}
-          #   export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-          #   export EXTRA_CCFLAGS="-I/usr/include"
-          #   export PATH=${cudatoolkit}/bin:${pkgs.gcc12}/bin:$PATH
-          # '';
-
         };
       }
     );
